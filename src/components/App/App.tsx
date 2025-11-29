2a521be3086678742fea2470c9b87f19a0760ac8
Sandra Hotsyn <sandrahotsy@gmail.com>
2025-11-29 20:43:55 +0200

    add components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+import css from "./App.module.css";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+// import type { Votes, VoteType } from "../../types/votes";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [votes, setVotes] = useState<Votes>({
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0,
+  // });
+
+  // const resetVotes = () => {
+  //   setVotes({
+  //     good: 0,
+  //     neutral: 0,
+  //     bad: 0,
+  //   });
+  // };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={css.app}>
+        <CafeInfo />
+        <VoteOptions />
+        <VoteStats />
+        <Notification />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
